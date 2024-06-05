f17366e3588a227e0caa6dfd4e83bbb59e55639b
KHITER Med Achraf <khiter.med7@gmail.com>
2024-06-05 20:57:53 +0200

    Update the UI to finalise the User experience

--- frontend/app.js
+++ frontend/app.js
@@ -1,22 +1,45 @@
+$(document).ready(function() {
+    $('#user-input').keypress(function(event) {
+        if (event.keyCode === 13) {
+            sendMessage();
+        }
+    });
+
+    $('#darkModeToggle').change(function() {
+        if ($(this).is(':checked')) {
+            $('body').addClass('dark-mode');
+        } else {
+            $('body').removeClass('dark-mode');
+        }
+    });
+});
+
 function sendMessage() {
-    const userInput = document.getElementById('user-input').value;
+    const userInput = $('#user-input').val();
     if (userInput.trim() === '') return;
 
-    const chatLog = document.getElementById('chat-log');
-    chatLog.innerHTML += `<div><strong>You:</strong> ${userInput}</div>`;
+    const chatLog = $('#chat-log');
+    chatLog.append(`<div><strong>You:</strong> ${userInput}</div>`);
+    $('#user-input').val('');
 
-    fetch('http://127.0.0.1:5000/chatbot', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ query: userInput })
-    })
-    .then(response => response.json())
-    .then(data => {
-        chatLog.innerHTML += `<div><strong>ACS_Bot:</strong> ${data.response}</div>`;
-        document.getElementById('user-input').value = '';
-        chatLog.scrollTop = chatLog.scrollHeight;
-    })
-    .catch(error => console.error('Error:', error));
+    const thinkingIndicator = $('<div class="thinking"><img src="3dots.gif" alt="Thinking..."></div>');
+    chatLog.append(thinkingIndicator);
+    chatLog.scrollTop(chatLog.prop("scrollHeight"));
+
+    setTimeout(() => {
+        fetch('http://127.0.0.1:5000/chatbot', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ query: userInput })
+        })
+        .then(response => response.json())
+        .then(data => {
+            thinkingIndicator.remove();
+            chatLog.append(`<div><strong>Bot:</strong> ${data.response}</div>`);
+            chatLog.scrollTop(chatLog.prop("scrollHeight"));
+        })
+        .catch(error => console.error('Error:', error));
+    }, 2000); // Simulate thinking time
 }
